cb58356d5bf1b09c8707bd1d94b290fa5eb9f1df
nguyenhuusy <52096108+nguyenhuusy@users.noreply.github.com>
2020-02-29 22:56:03 +0700

    Updates

---
+++ precache-manifest.28cba2548b8945baf0fc201892b408c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38c6be7f3cab7d8a0c12521daf17542",
+    "url": "/trollo/index.html"
+  },
+  {
+    "revision": "1931f55a9a1b61925af4",
+    "url": "/trollo/static/css/main.3870ebbe.chunk.css"
+  },
+  {
+    "revision": "45b27b46ae3ca86ffd6e",
+    "url": "/trollo/static/js/2.85fa66ac.chunk.js"
+  },
+  {
+    "revision": "d1ab45a26fc44db7b0f9fbdc6de8debb",
+    "url": "/trollo/static/js/2.85fa66ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1931f55a9a1b61925af4",
+    "url": "/trollo/static/js/main.437e58ee.chunk.js"
+  },
+  {
+    "revision": "5e8877f5bb14fc47cbb6",
+    "url": "/trollo/static/js/runtime-main.30b51bf7.js"
+  }
+]);
